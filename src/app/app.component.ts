c6bc8ff0b2785801b6f1becfd24d3cd96a96540b
Suraj Poddar <surajpoddar16@gmail.com>
2017-05-09 19:17:44 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../assets/sass/styles.scss';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({
+      appId: 'toh-universal'
+    }),
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
