7904b836a7f04498e0fa0b7d40056f49a06982bb
Suraj Poddar <surajpoddar16@gmail.com>
2017-07-09 10:40:49 +0530

    Chat panel WIP

---
+++ src/app/chat-list-panel/chat-contact-list/chat_contact_list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-contact-list',
+  templateUrl: './chat_contact_list.component.html',
+  styleUrls: ['./chat_contact_list.component.scss']
+})
+export class ChatContactListComponent { }
---
+++ src/app/chat-pane/chat_pane.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-pane',
+  templateUrl: './chat_pane.component.html',
+  styleUrls: ['./chat_pane.component.scss']
+})
+export class ChatPaneComponent { }
