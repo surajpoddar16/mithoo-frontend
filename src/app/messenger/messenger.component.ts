df62611e832478bd995089ecb7521d35a1ad2aa4
Suraj Poddar <surajpoddar16@gmail.com>
2017-07-09 10:10:33 +0530

    Messenger component initialised

---
+++ src/app/messenger/messenger.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'messenger',
+  templateUrl: './messenger.component.html',
+  styleUrls: ['./messenger.component.scss']
+})
+export class MessengerComponent { }
